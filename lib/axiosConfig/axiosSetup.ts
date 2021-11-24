6298b39add35244d59ac2854095487e8414ee8e1
Daniel <bdcaldera@gmail.com>
2021-11-23 20:01:08 -0600

    saved IMGUR TOKEN

--- lib/axiosConfig/axiosSetup.ts
+++ lib/axiosConfig/axiosSetup.ts
@@ -4,6 +4,6 @@ export const axiosInstance = axios.create({
   baseURL: 'https://api.imgur.com/',
   timeout: 1000,
   headers: {
-    Authorization: 'Bearer ' + 'd611872356a1a8421c74561e9c1e0e3db41f0d9f',
+    Authorization: 'Bearer ' + process.env.NEXT_PUBLIC_IMGUR_TOKEN,
   },
 })
